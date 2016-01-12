31ffe8966313af3393b3c3ddead5643410880603
Tom Ruttle <tom@tomruttle.com>
2016-01-12 10:39:33 +0000

    actually export an array of domains

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports = require('./disposable_email_blacklist');
+var listPath = require('path').join(__dirname, 'disposable_email_blacklist.conf');
+var blacklist = require('fs').readFileSync(listPath, 'utf8');
+module.exports = blacklist.split('\n').filter(Boolean);
